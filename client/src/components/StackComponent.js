94195fc564e12017928424b93123b3f556b8e750
trojanh <rajan.tiwari@kiprosh.com>
2020-08-28 20:04:22 +0530

    Add posts hook with infinite scroll

--- client/src/components/StackComponent.js
+++ client/src/components/StackComponent.js
@@ -1,58 +1,30 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import StackGrid from 'react-stack-grid'
 import { useBottomScrollListener } from 'react-bottom-scroll-listener'
-import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
 import { StackItem } from './StackItem'
 import AddStackItem from './AddStackItem'
-
-const GET_POSTS_QUERY = gql`
-  {
-    fetchAllPosts(requests: { cursor: { page: 0, limit: 10 } }) {
-      success
-      data {
-        _id
-        name
-        description
-        createdAt
-        author {
-          name
-        }
-      }
-      error {
-        status
-        message
-      }
-    }
-  }
-`
+import { usePosts } from '../hooks'
 
 const StackComponent = () => {
-  const { data, loading, error } = useQuery(GET_POSTS_QUERY)
+  const { posts: allPosts, loading, loadMore, hasMore } = usePosts(5)
+  const [page, updatePage] = useState(0)
   const handleOnDocumentBottom = useCallback(() => {
-    console.log('I am at bottom! ' + Math.round(performance.now()))
-
-      alert('Bottom hit!')
-  }, [])
+    if (hasMore) {
+      updatePage(page + 1)
+      loadMore(page + 1)
+    }
+    console.log({ hasMore, page })
+  }, [page, hasMore])
   useBottomScrollListener(handleOnDocumentBottom)
-
 
   if (loading) {
     return <p>Loading ...</p>
   }
-  if (error) {
-    return <p>{error.message}</p>
-  }
 
-
-
-  const {
-    fetchAllPosts: { data: allPosts }
-  } = data
-  console.log({ allPosts })
+  console.log({ allPosts, hasMore })
   return (
     <StackGrid columnWidth={200}>
-      <AddStackItem/>
+      <AddStackItem />
       {allPosts.map((post) => (
         <StackItem key={post._id} post={post} />
       ))}
